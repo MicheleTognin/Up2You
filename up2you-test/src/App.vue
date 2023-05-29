f08a9e807f1b62a41bd711a50e6b2a6b757523de
Michele Tognin <michele.tognin@gmail.com>
2023-05-29 18:29:56 +0200

    wip
    wip

--- up2you-test/src/App.vue
+++ up2you-test/src/App.vue
@@ -4,6 +4,15 @@ import NftTable from './components/NftTable.vue';
 </script>
 
 <template>
+  <header class="title-background">
+    <div class="section-header">
+      <h1>Carbon Credits su Blockchain</h1>
+    <h2>Crediti di carbonio certificati su Blockchain</h2>
+    </div>
+    <div class="section-header">
+      <img src="./assets/images/titolo.png" alt="titolo" class=""/>
+    </div>
+  </header>
   <NftTable></NftTable>
 </template>
 
--- up2you-test/src/components/NftTable.vue
+++ up2you-test/src/components/NftTable.vue
@@ -1,4 +1,8 @@
 <template>
+    <div>
+        <input type="text" v-model="search" placeholder="Cerca" />
+    </div>
+
     <table class="table-auto">
         <thead>
             <tr>
@@ -14,7 +18,7 @@
         </thead>
         <tbody>
             <tr v-for="nft in sortedNfts" :key="nft.id">
-                <td><img :src="nft.image"/></td>
+                <td><img :src="nft.image" alt="Nft Image" class="w-24 h-24" /></td>
                 <td>{{ nft.internal_id }}</td>
                 <td>{{ nft.details.entity_name }}</td>
                 <td>{{ nft.formatted_quantity_kg }}</td>
@@ -26,8 +30,20 @@
         </tbody>
     </table>
     <div>
-        <button @click="prevPage">Previous</button>
-        <button @click="nextPage">Next</button>
+        <button @click="prevPage">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M16.2071 4.29289C16.5976 4.68342 16.5976 5.31658 16.2071 5.70711L9.91421 12L16.2071 18.2929C16.5976 18.6834 16.5976 19.3166 16.2071 19.7071C15.8166 20.0976 15.1834 20.0976 14.7929 19.7071L7.79289 12.7071C7.40237 12.3166 7.40237 11.6834 7.79289 11.2929L14.7929 4.29289C15.1834 3.90237 15.8166 3.90237 16.2071 4.29289Z"
+                    fill="#3F4851" />
+            </svg>
+        </button>
+        <button @click="nextPage"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M7.79289 19.7071C7.40237 19.3166 7.40237 18.6834 7.79289 18.2929L14.0858 12L7.79289 5.70711C7.40237 5.31658 7.40237 4.68342 7.79289 4.29289C8.18342 3.90237 8.81658 3.90237 9.20711 4.29289L16.2071 11.2929C16.5976 11.6834 16.5976 12.3166 16.2071 12.7071L9.20711 19.7071C8.81658 20.0976 8.18342 20.0976 7.79289 19.7071Z"
+                    fill="#3F4851" />
+            </svg>
+        </button>
     </div>
 </template>
 
@@ -73,3 +89,5 @@ export default {
 }
 
 </script>
+
+<style></style>
